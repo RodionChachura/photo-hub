3b9e799c6c73223d0923efee1cd1d13a1f81d1fd
RodionChachura <rodionaaafgh@gmail.com>
2016-10-09 13:18:04 +0300

    switched to postgresql

--- photo-hub/photo_hub/settings.py
+++ photo-hub/photo_hub/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'photo_hub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'photo_hub_db',
+        'USER': 'photo_hub_admin',
+        'PASSWORD': '1B3OxQNuGz',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -133,3 +138,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_media')
+MEDIA_URL = '/media/'
